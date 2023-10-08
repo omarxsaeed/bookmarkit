84e8e9d76e3a2730e3a8f070c366f1812d791753
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2023-10-08 17:47:33 +0300

    add: get current user endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
